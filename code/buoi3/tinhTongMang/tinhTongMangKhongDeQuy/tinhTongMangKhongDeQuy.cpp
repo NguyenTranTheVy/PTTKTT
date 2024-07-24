a4cbc1521cfa33bbb7ec12e5d014bf69933df508
PhanTichThietKeThuatToan <vinguyen10012003@gmail.com>
2024-07-24 17:57:21 +0700

    main

---
+++ code/buoi3/tinhTongMang/tinhTongMangKhongDeQuy/tinhTongMangKhongDeQuy.cpp
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int sumArray(int arr[], int n) {
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+int main() {
+    int n = 5;
+    int arr[] = { 1, 2, 3, 4, 5 }; // Mảng từ 1 đến n
+    int sum = sumArray(arr, n);
+    printf("Tổng các phần tử trong mảng là: %d\n", sum);
+    return 0;
+}
+
+
