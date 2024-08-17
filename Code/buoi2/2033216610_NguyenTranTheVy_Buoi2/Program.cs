276d9c02e297e3c1bcb92e20f8210fa45dbed5ef
PhanTichThietKeThuatToan <vinguyen10012003@gmail.com>
2024-08-18 00:11:43 +0700

    initial commit

---
+++ Code/buoi2/2033216610_NguyenTranTheVy_Buoi2/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _2033216610_NguyenTranTheVy_Buoi2
+{
+    using System;
+
+    class Program
+    {
+        static int timKiemNhinPhan(int[] mang, int n, int key)
+        {
+            int left = 0; 
+            int right = n - 1; 
+            while (left <= right)
+            {
+                int mid = (left + right) / 2; 
+                if (mang[mid] == key)
+                {
+                    return mid;
+                }
+                if (key < mang[mid])
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+            return -1;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] mang = { 10, 20, 30, 40, 50 };
+            int n = mang.Length; 
+            int key = 40; 
+            int result = timKiemNhinPhan(mang, n, key);
+            if (result != -1)
+            {
+                Console.WriteLine("Phần tử được tìm thấy ở vị trí: " + result);
+            }
+            else
+            {
+                Console.WriteLine("Phần tử không có trong mảng");
+            }
+        }
+    }
+}
